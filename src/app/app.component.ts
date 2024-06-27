c5dbb64e72224d6eac96e618abb121811ff68315
Kenneth <kwbonte@gmail.com>
2024-06-27 12:19:57 -0500

    Getting the generated code to display in an completely unmodified manner to quickly buidl out functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoListComponent, TodoListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
